e22c02f33b60ffed86e76013dc504f40f2b3c67f
maeda <maeda@illi-ichi.com>
2019-07-06 07:30:03 +0900

    project

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject orenolisp "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.openjfx/javafx-controls "11.0.1"]
+                 [org.openjfx/javafx-swing "11.0.1"]
+                 [org.openjfx/javafx-graphics "11.0.1"]
+                 [org.clojure/core.async "0.4.490"]
+                 [org.clojure/core.match "0.3.0"]
+                 [llll "0.1.1"]]
+  :jvm-opts ^:replace [])
