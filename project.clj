64811d0476f2c43267882e5800aee8dcb2e05b4e
maeda <maeda@illi-ichi.com>
2019-07-29 01:35:48 +0900

    change javafx version

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.openjfx/javafx-controls "11.0.1"]
-                 [org.openjfx/javafx-swing "11.0.1"]
-                 [org.openjfx/javafx-graphics "11.0.1"]
+                 [org.openjfx/javafx-controls "11.0.2"]
+                 [org.openjfx/javafx-swing "11.0.2"]
+                 [org.openjfx/javafx-graphics "11.0.2"]
                  [org.clojure/core.async "0.4.490"]
                  [org.clojure/core.match "0.3.0"]
                  [llll "0.1.1"]]
