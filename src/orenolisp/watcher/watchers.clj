7210f64189301dbf2cea0a390e3826eafd960555
maeda <maeda@illi-ichi.com>
2019-08-14 06:43:43 +0900

    reduce 0 div

--- src/orenolisp/watcher/watchers.clj
+++ src/orenolisp/watcher/watchers.clj
@@ -4,15 +4,23 @@
             [orenolisp.util :as u]))
 
 (defn gauge-watcher [exp-id node-id exp? from to]
-  (fn [x]
-    (when (not (nil? x))
-      (let [r (/ (- x from) (- to from))]
-        (if (u/enough-equal r 1.0)
-          (cmd/excursion exp-id node-id
-                         #(-> %
-                              (ed/move [:child :right])
-                              (ed/transport :self node-id)))
-          (cmd/edit-attributes exp-id node-id #(assoc % :ratio r)))))))
+  (if (= from to)
+    (fn [x]
+      (cmd/excursion exp-id node-id
+                     #(-> %
+                          (ed/move [:child :right])
+                          (ed/transport :self node-id))))
+    (fn [x]
+      (when (not (nil? x))
+        (let [r (/ (- x from) (- to from))]
+          (if (u/enough-equal r 1.0)
+            (cmd/excursion exp-id node-id
+                           #(-> %
+                                (ed/move [:child :right])
+                                (ed/transport :self node-id)))
+            (cmd/edit-attributes exp-id node-id #(-> %
+                                                     (assoc :ratio r)
+                                                     (assoc :value x)))))))))
 
 (defn create-gauge-watcher [{:keys [exp-id editor]} node-id]
   (let [exp? (:exp? (ed/get-content editor))
